a1068f60a9ae301ed16c1d44a0e38bad55b27960
Roi Kingon <rkingon@gmail.com>
2017-07-10 21:31:16 -0700

    in hopes to turn off verification of self signed cert

--- lib/devise_ldap_authenticatable/ldap/connection.rb
+++ lib/devise_ldap_authenticatable/ldap/connection.rb
@@ -4,14 +4,24 @@ module Devise
       attr_reader :ldap, :login
 
       def initialize(params = {})
+        
         if ::Devise.ldap_config.is_a?(Proc)
           ldap_config = ::Devise.ldap_config.call
         else
           ldap_config = YAML.load(ERB.new(File.read(::Devise.ldap_config || "#{Rails.root}/config/ldap.yml")).result)[Rails.env]
         end
+        
         ldap_options = params
-        ldap_config["ssl"] = :simple_tls if ldap_config["ssl"] === true
-        ldap_options[:encryption] = ldap_config["ssl"].to_sym if ldap_config["ssl"]
+        
+        if ldap_config["ssl"] === true
+          ldap_config["ssl"] = :simple_tls
+          ldap_options[:encryption] = {
+            method: :simple_tls,
+            tls_options: {
+              verify_mode: OpenSSL::SSL::VERIFY_NONE
+            }
+          }
+        end
 
         @ldap = Net::LDAP.new(ldap_options)
         @ldap.host = ldap_config["host"]
